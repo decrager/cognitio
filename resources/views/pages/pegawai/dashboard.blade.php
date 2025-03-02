667e74869b5fbe72c5de1b6b021af956ed5357a1
ahmdsfdn <saifudinahmad439@gmail.com>
2025-03-03 01:47:48 +0700

    front: layouting dashboard

--- resources/views/pages/pegawai/dashboard.blade.php
+++ resources/views/pages/pegawai/dashboard.blade.php
@@ -3,71 +3,52 @@
 @section('title', 'Dashboard Pegawai')
 
 @section('content')
-<!-- Small boxes (Stat box) -->
 <div class="row">
-    <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-green">
-            <div class="inner">
-                <h3>{{ $jumlah_kpi_cukup }}</h3>
+    <div class="col-lg-4 col-md-12 col-xs-12 d-flex align-item-stretch">
+        <div class="info-box flex-fill">
+            <span class="info-box-icon bg-yellow mr-4 h-100 d-flex align-items-center justify-content-center" style="width: 150px"><i class="ion ion-ios-person-outline"></i></span>
 
-                <p>Kompentensi Pegawai</p>
+            <div class="info-box-content ml-4">
+                <div class="col-lg-12 col-md-12 col-xs-12">
+                    <h3 class="text-bold">{{$pegawai->nama}}</h3>
+                    <span class="info-box-text text-bold">{{$pegawai->jabatan->nama_jabatan}}</span>
+                    <span class="info-box-text">NIP {{$pegawai->nip}}</span>
+                    <span class="info-box-text">NIK {{$pegawai->nik}}</span>
+                </div>
             </div>
-            <div class="icon">
-                <i class="ion ion-bag"></i>
-            </div>
-            <a href="#" class="small-box-footer">Cukup <i class="fa fa-arrow-circle-right"></i></a>
+            <!-- /.info-box-content -->
         </div>
     </div>
-    <!-- ./col -->
-    <div class="col-lg-3 col-xs-6">
+    <div class="col-lg-4 col-md-12 col-xs-12 d-flex align-items-stretch">
         <!-- small box -->
-        <div class="small-box bg-red">
+        <div class="small-box bg-blue flex-fill">
             <div class="inner">
-                <h3>{{ $jumlah_kpi_kurang }}<sup style="font-size: 20px"></sup></h3>
+                <h3>{{ $jumlah_kpi_cukup }} / {{$jumlah_kpi_kurang}}</h3>
 
-                <p>Kompetensi Pegawai</p>
+                <h5>Kompetensi Pegawai</h5>
+
+                <div class="progress">
+                    <div class="progress-bar bg-success" style="width: {{($jumlah_kpi_cukup  / $jumlah_kpi_kurang + $jumlah_kpi_cukup) * 100}}%"></div>
+                </div>
             </div>
             <div class="icon">
-                <i class="ion ion-stats-bars"></i>
+                <i class="ion ion-laptop"></i>
             </div>
-            <a href="#" class="small-box-footer">Kurang <i class="fa fa-arrow-circle-right"></i></a>
         </div>
     </div>
-    <!-- ./col -->
-    <!-- <div class="col-lg-3 col-xs-6"> -->
+    <div class="col-lg-4 col-md-12 col-xs-12 d-flex align-items-stretch">
         <!-- small box -->
-        <!-- div class="small-box bg-yellow">
+        <div class="small-box bg-warning flex-fill">
             <div class="inner">
-                <h3>44</h3>
-
-                <p>User Registrations</p>
+                <h3>{{ $jumlah_assignment_ditetapkan + $jumlah_assignment_konfirmasi + $jumlah_assignment_usulan + $jumlah_assignment_tidak_dikonfirmasi}}</h3>
+                <h5>Total Pelatihan</h5>
             </div>
             <div class="icon">
-                <i class="ion ion-person-add"></i>
+                <i class="ion ion-folder"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
         </div>
-    </div> -->
-    <!-- ./col -->
-    <!-- <div class="col-lg-3 col-xs-6"> -->
-        <!-- small box -->
-        <!-- <div class="small-box bg-red">
-            <div class="inner">
-                <h3>65</h3>
-
-                <p>Unique Visitors</p>
-            </div>
-            <div class="icon">
-                <i class="ion ion-pie-graph"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-        </div>
-    </div> -->
-    <!-- ./col -->
+    </div>
 </div>
-<!-- /.row -->
-
 <div class="row">
     <div class="col-lg-3 col-xs-6">
         <!-- small box -->
@@ -75,12 +56,11 @@
             <div class="inner">
                 <h3>{{ $jumlah_assignment_usulan }}</h3>
 
-                <p>Usulan Pelatihan</p>
+                <p>Pelatihan yang Di Usulkan</p>
             </div>
             <div class="icon">
-                <i class="ion ion-bag"></i>
+                <i class="ion ion-information-circled"></i>
             </div>
-            <a href="#" class="small-box-footer">Cukup <i class="fa fa-arrow-circle-right"></i></a>
         </div>
     </div>
     <!-- ./col -->
@@ -90,12 +70,11 @@
             <div class="inner">
                 <h3>{{ $jumlah_assignment_konfirmasi }}<sup style="font-size: 20px"></sup></h3>
 
-                <p>Konfirmasi Pelatihan</p>
+                <p>Pelatihan Pending Konfirmasi</p>
             </div>
             <div class="icon">
-                <i class="ion ion-stats-bars"></i>
+                <i class="ion ion-help-circled"></i>
             </div>
-            <a href="#" class="small-box-footer">Kurang <i class="fa fa-arrow-circle-right"></i></a>
         </div>
     </div>
     <!-- ./col -->
@@ -105,12 +84,11 @@
             <div class="inner">
                 <h3>{{ $jumlah_assignment_tidak_dikonfirmasi }}</h3>
 
-                <p>Tidak dikonfirmasi Pelatihan</p>
+                <p>Pelatihan Tidak Terkonfirmasi</p>
             </div>
             <div class="icon">
-                <i class="ion ion-person-add"></i>
+                <i class="ion ion-thumbsdown"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
         </div>
     </div>
     <!-- ./col -->
@@ -120,12 +98,11 @@
             <div class="inner">
                 <h3>{{ $jumlah_assignment_ditetapkan }}</h3>
 
-                <p>Ditetapkan Pelatihan</p>
+                <p>Pelatihan Terkonfirmasi</p>
             </div>
             <div class="icon">
-                <i class="ion ion-pie-graph"></i>
+                <i class="ion ion-thumbsup"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
         </div>
     </div>
     <!-- ./col -->
@@ -133,72 +110,12 @@
 <!-- /.row -->
 
 <div class="row">
-    
-    <div class="col-md-3">
+    <div class="col-md-12">
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title">Profil Pegawai</h3>
-            </div>
-            <!-- /.card-header -->
-            <div class="card-body p-0">
-                <table class="table table-striped">
-                    <tbody>
-                        <tr>
-                            <td>ID User</td>
-                            <td>{{ $pegawai->id ?? '-' }}</td>
-                        </tr>
-                        <tr>
-                            <td>ID Jabatan</td>
-                            <td>{{ $pegawai->jabatan->nama_jabatan ?? '-' }}</td>
-                        </tr>
-                        <tr>
-                            <td>ID Unit</td>
-                            <td>{{ $pegawai->unit->nama_unit ?? '-' }}</td>
-                        </tr>
-                        <tr>
-                            <td>Nama</td>
-                            <td>{{ $pegawai->nama ?? '-' }}</td>
-                        </tr>
-                        <tr>
-                            <td>NIP</td>
-                            <td>{{ $pegawai->nip ?? '-' }}</td>
-                        </tr>
-                        <tr>
-                            <td>NIK</td>
-                            <td>{{ $pegawai->nik ?? '-' }}</td>
-                        </tr>
-                        <tr>
-                            <td>Telepon</td>
-                            <td>{{ $pegawai->telepon ?? '-' }}</td>
-                        </tr>
-                        <tr>
-                            <td>Tanggal Lahir</td>
-                            <td>{{ $pegawai->tanggal_lahir ?? '-' }}</td>
-                        </tr>
-                        <tr>
-                            <td>Jenis Kelamin</td>
-                            <td>{{ $pegawai->jenis_kelamin ?? '-' }}</td>
-                        </tr>
-                        <tr>
-                            <td>Alamat</td>
-                            <td>{{ $pegawai->alamat ?? '-' }}</td>
-                        </tr>
-                        <tr>
-                            <td>Tipe</td>
-                            <td>{{ $pegawai->tipe ?? '-' }}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <!-- /.card-body -->
-        </div>
-        <!-- /.card -->
-    </div>
-
-    <div class="col-md-9">
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Kompetensi Pegawai</h3>
+                <h4 class="card-title text-bold">
+                   <i class="fa fa-info mr-2"></i> Kompetensi Pegawai
+                </h4>
             </div>
             <!-- /.card-header -->
             <div class="card-body p-0">
@@ -206,9 +123,7 @@
                     <table class="table table-striped">
                         <thead>
                             <tr>
-                                <th>#</th>
-                                <th>ID Pegawai</th>
-                                <th>ID Jabatan</th>
+                                <th>No</th>
                                 <th>Nama Kompetensi</th>
                                 <th>KPI Standar</th>
                                 <th>KPI Pegawai</th>
@@ -219,12 +134,13 @@
                             $kpi_pegawai = $item->kpi ?? 0;
                             $kpi_standar = $item->standarKompetensi->kpi_standar ?? 0;
                             $warna_kpi = $kpi_pegawai >= $kpi_standar ? 'color:green;font-weight:bold;' : 'color:red;font-weight:bold;';
+
+                            $class_bg = $kpi_pegawai < $kpi_standar  ? 'bg-warning' : '';
+
                         @endphp
                             <tbody>
-                                <tr>
+                                <tr class="{{$class_bg}}">
                                     <td>{{ $loop->iteration }}</td>
-                                    <td>{{ $item->id_pegawai ?? '-' }}</td>
-                                    <td>{{ $item->standarKompetensi->jabatan->nama_jabatan ?? '-' }}</td>
                                     <td>{{ $item->standarKompetensi->nama_kompetensi ?? 'Tidak Ada' }}</td>
                                     <td>{{ $item->standarKompetensi->kpi_standar ?? '-' }}</td>
                                     <td style="{{ $warna_kpi }}">{{ $kpi_pegawai }}</td>
@@ -242,10 +158,12 @@
     </div>
 
 
-    <div class="col-12">
+    <div class="col-12 mt-4">
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title">Jadwal Pelatihan</h3>
+                <h4 class="card-title text-bold">
+                    <i class="fa fa-info mr-2"></i> Jadwal Pelatihan
+                </h4>
             </div>
             <!-- /.card-header -->
             <div class="card-body">
@@ -253,19 +171,15 @@
                     <table id="example1" class="table table-bordered table-striped">
                         <thead>
                             <tr>
-                                <th>#</th>
-                                <th>ID Pegawai</th>
-                                <th>status</th>
-                                <th>ID Program</th>
+                                <th>No</th>
                                 <th>Nama Pelatihan</th>
+                                <th>Status</th>
                                 <th>Deskripsi</th>
                                 <th>Tanggal Mulai</th>
                                 <th>Tanggal Selesai</th>
-                                <th>Kuota</th>
                                 <th>Lokasi</th>
                                 <th>Penyelenggara</th>
-                                <th>Assigned By ID</th>
-                                <th>Assigned By Name</th>
+                                <th>Di Usulkan Oleh</th>
                             </tr>
                         </thead>
                         @foreach($assignment as $item2)
@@ -298,10 +212,8 @@
                                     <td>{{ $item2->Program->deskripsi ?? '-' }}</td>
                                     <td>{{ $item2->Program->tanggal_mulai ?? '-' }}</td>
                                     <td>{{ $item2->Program->tanggal_selesai ?? '-' }}</td>
-                                    <td>{{ $item2->Program->kuota ?? '-' }}</td>
                                     <td>{{ $item2->Program->lokasi ?? '-' }}</td>
                                     <td>{{ $item2->Program->penyelenggara ?? '-' }}</td>
-                                    <td>{{ $item2->assigned_by_id ?? '-' }}</td>
                                     <td>{{ $item2->assigned_by_name ?? '-' }}</td>
                                 </tr>
                             </tbody>
